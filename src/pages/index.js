860f7530ff1a579d4f846e87bd7ee88fea2901eb
Shridharann41 <shridharan41@gmail.com>
2024-10-18 21:07:02 +0530

    navbar push

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Home from "./Home";
+import About from './About'
+import Projects from "./Projects";
+import Contact from "./Contact";
+import Navbar from "Navbar";
+
+export {
+  
+    Home,
+    About,
+    Projects,
+    Contact,
+    Navbar
+    
+}

34781274b5cd6623b5ff294b6c411a11872cb974
Shridharann41 <shridharan41@gmail.com>
2024-11-13 13:44:18 +0530

    Contact commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
 import Navbar from "./Navbar";
+import Alert from "./Alert";
+import Loader from "./Loader";
 
 export  {Navbar}
+export {Loader}
+export {Alert}
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import emailjs from '@emailjs/browser'
 import { Suspense, useRef, useState } from "react";
+import { Canvas } from '@react-three/fiber';
+import {Fox} from '../models/Fox';
+import Loader  from '../components/Loader';
+import Alert from '../components/Alert';
 
 const Contact = () => {
   const formRef = useRef(null);
   const [form, setForm] = useState({name:"", email: "", message: ""})
   const [isLoading, setIsLoading] = useState(false);
+  const [currentAnimation, setCurrentAnimation] = useState('idle')
+
   const handleChange = (e) => {
     setForm({...form, [e.target.name]: e.target.value})
   };
-  const handleFocus = () => {};
-  const handleBlur = () => {};
+  const handleFocus = () => setCurrentAnimation('walk');
+  const handleBlur = () => setCurrentAnimation('idle')
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsLoading(true);
-    console.log(import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-    )
+    setCurrentAnimation('hit');
+    
     emailjs.send(
       import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
       import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
@@ -29,21 +35,29 @@ const Contact = () => {
       import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY 
     ).then(() => {
       setIsLoading(false);
-      //TODO: Show Success message
-      //TODO: Hide an alert
+      showAlert({show: true, text: 'Message sent successfully! type: success'})
+      
+
+      setTimeout(() => {
+        hideAlert();
+        setCurrentAnimation('idle')
+        setForm({name: '', email: '', message: ''})
+      }, [3000])
 
       setForm({name: '', email: '', message: ''});
-
-
     }).catch((error) => {
       setIsLoading(false);
+      setCurrentAnimation('idle')
       console.log(error);
-      // TODO: Show error Message
+      showAlert({show: true, text: 'i didnt receive your message', type: 'danger'})
+      
 
     })
   };
   return (
     <section className='relative flex lg:flex-row flex-col max-container'>
+      {alert.show && <Alert{...alert}/>}
+
       <div className='flex-1 min-w-[50%] flex flex-col'>
         <h1 className='head-text'>
           Get in Touch
@@ -101,12 +115,29 @@ const Contact = () => {
           >
             {isLoading ? "Sending..." : "Submit"}
           </button>
-
-
         </form>
-
       </div>
+      <div className='lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]'>
+      <Canvas
+      camera={{
+        position: [0,0,5],
+        fov: 75,
+        near: 0.1,
+        far: 1000
+      }}>
+        <directionalLight intensity={2.5} position={[0, 0, 1]}/>
+        <ambientLight intensity={0.5} />
+        <Suspense fallback={<Loader/>}>
+          <Fox 
+          currentAnimation={currentAnimation}
+          position={[0.5, 0.35, 0]}
+          rotation={[12.6,-0.6,0]}
+          scale={[0.5, 0.5, 0.5]}
+          />
+         </Suspense>
 
+      </Canvas>
+      </div>
     </section>
   )
 }
